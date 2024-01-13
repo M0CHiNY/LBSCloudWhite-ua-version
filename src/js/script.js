47c1523be96dfdd047f74c9f2d192a776be756d2
Volodymyr <volodimrmihnyk@gmail.com>
2024-01-13 15:23:54 +0200

    add lang and btn select lang

--- src/js/script.js
+++ src/js/script.js
@@ -1,44 +1,41 @@
 document.addEventListener("DOMContentLoaded", function () {
   // acordeon
-// const tabs = document.querySelectorAll("[data-tab]");
-// const tabsContent = document.querySelectorAll("[data-tab-content]");
 
-// tabs.forEach(function (item) {
-//   item.addEventListener("click", function () {
-//     const currentBtn = item;
-//     const contentBox = document.querySelector("#" + this.dataset.tab);
+  // const tabs = document.querySelectorAll("[data-tab]");
+  // const tabsContent = document.querySelectorAll("[data-tab-content]");
 
-//     if (currentBtn.classList.contains("tab-active")) {
-//       contentBox.style.maxHeight = "0";
-//       setTimeout(() => {
-//         contentBox.style.display = "none";
-//       }, 500);
-//       currentBtn.classList.remove("tab-active");
-//     } else {
-//       tabs.forEach(function (item) {
-//         item.classList.remove("tab-active");
-//       });
+  // tabs.forEach(function (item) {
+  //   item.addEventListener("click", function () {
+  //     const currentBtn = item;
+  //     const contentBox = document.querySelector("#" + this.dataset.tab);
 
-//       currentBtn.classList.add("tab-active");
+  //     if (currentBtn.classList.contains("tab-active")) {
+  //       contentBox.style.maxHeight = "0";
+  //       setTimeout(() => {
+  //         contentBox.style.display = "none";
+  //       }, 500);
+  //       currentBtn.classList.remove("tab-active");
+  //     } else {
+  //       tabs.forEach(function (item) {
+  //         item.classList.remove("tab-active");
+  //       });
 
-//       tabsContent.forEach(function (e) {
-//         if (e !== contentBox) {
-//           e.style.maxHeight = 0;
-//           setTimeout(() => {
-//             e.style.display = "none";
-//           }, 500);
-//         }
-//       });
+  //       currentBtn.classList.add("tab-active");
 
-//       contentBox.style.display = "block";
-//       contentBox.style.maxHeight = contentBox.scrollHeight + "px";
-//     }
-//   });
-// });
+  //       tabsContent.forEach(function (e) {
+  //         if (e !== contentBox) {
+  //           e.style.maxHeight = 0;
+  //           setTimeout(() => {
+  //             e.style.display = "none";
+  //           }, 500);
+  //         }
+  //       });
 
-
-
-
+  //       contentBox.style.display = "block";
+  //       contentBox.style.maxHeight = contentBox.scrollHeight + "px";
+  //     }
+  //   });
+  // });
 
   // scroll
 
@@ -58,36 +55,32 @@ document.addEventListener("DOMContentLoaded", function () {
       scrollToTop.style.display = "none";
     }
   }
+
   //modal
-document.addEventListener("click", (e) => {
-  const openModalButton = e.target.closest(".prices__btn");
 
-  if (openModalButton) {
-    const modalId = openModalButton.getAttribute("data-modal-id");
-    const modal = document.getElementById(modalId);
+  document.addEventListener("click", (e) => {
+    const openModalButton = e.target.closest(".prices__btn");
 
-    if (modal) {
-      modal.showModal();
+    if (openModalButton) {
+      const modalId = openModalButton.getAttribute("data-modal-id");
+      const modal = document.getElementById(modalId);
 
-      modal.addEventListener("click", (e) => {
-        if (e.target === modal) modal.close();
-      });
+      if (modal) {
+        modal.showModal();
 
-      const closeButton = modal.querySelector(".modal__btn-clouse");
-
-      if (closeButton) {
-        closeButton.addEventListener("click", () => {
-          modal.close();
+        modal.addEventListener("click", (e) => {
+          if (e.target === modal) modal.close();
         });
+
+        const closeButton = modal.querySelector(".modal__btn-clouse");
+
+        if (closeButton) {
+          closeButton.addEventListener("click", () => {
+            modal.close();
+          });
+        }
       }
     }
-  }
-});
-
-
-
-
-
-
-
+  });
+  
 });
